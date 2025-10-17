bce8e526576d2699e445e57ce5fdb55443c60c2e
Josh <josh@ursacreative.ca>
2025-10-17 15:46:21 -0700

    Fixed movement

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -3,13 +3,11 @@ extends CharacterBody2D
 @export var SPEED:int = 300
 
 func _physics_process(_delta: float) -> void:
-  if not Game.can_player_move:
-    return
-
   var vector = Vector2(
     Input.get_action_strength("Right") - Input.get_action_strength("Left"),
     Input.get_action_strength("Down") - Input.get_action_strength("Up")
   ).normalized()
 
-  velocity = vector * SPEED
-  move_and_slide()
+  if Game.can_player_move:
+    velocity = vector * SPEED
+    move_and_slide()
