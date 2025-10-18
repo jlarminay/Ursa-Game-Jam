c1c3cb3a9bc790edc13b56c35aa0f049285a7735
Nigel <nigel@ursacreative.ca>
2025-10-18 15:49:29 -0700

    Add pickup and drop

--- Entities/Item/item.gd
+++ Entities/Item/item.gd
@@ -1,19 +1,36 @@
 extends Area2D
 
 
+@export var regrab_threshold: int
 @export_enum("Carrot", "Flowers", "Honey", "Apple") var item: String
 @onready var _animated_sprite = $AnimatedSprite2D
-
+var player: CharacterBody2D
+var recently_dropped = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-  pass # Replace with function body.
+  player = get_tree().current_scene.find_child("Player")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
   match item:
     "Carrot": _animated_sprite.play("Carrot")
     "Flowers": _animated_sprite.play("Flowers")
     "Honey": _animated_sprite.play("Honey")
     "Apple": _animated_sprite.play("Apple")
+
+  # if distance > 5 then set rd false
+  if global_position.distance_to(player.global_position) > regrab_threshold:
+    recently_dropped = false
+
+func _on_body_entered(body: Node2D) -> void:
+  if recently_dropped:
+    return
+
+  if (not body.name == 'Player'):
+    return
+
+  if(body.held_item == "None"):
+    body.held_item = item
+    queue_free()
