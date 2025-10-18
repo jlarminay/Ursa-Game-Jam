090570c74a0098c4b4e7c907e48edb8378bfa8e4
Josh <josh@ursacreative.ca>
2025-10-17 18:41:03 -0700

    Minor

--- Global/AudioController/audio_controller.gd
+++ Global/AudioController/audio_controller.gd
@@ -7,35 +7,35 @@ extends Node2D
 var music_player: AudioStreamPlayer2D
 
 func _ready():
-	music_player = AudioStreamPlayer2D.new()
-	add_child(music_player)
+  music_player = AudioStreamPlayer2D.new()
+  add_child(music_player)
 
 func play_sound(sound: AudioStream):
-	if not sound:
-		return
+  if not sound:
+    return
 
-	var player = AudioStreamPlayer2D.new()
-	add_child(player)
-	player.stream = sound
-	player.play()
-	player.finished.connect(player.queue_free)
+  var player = AudioStreamPlayer2D.new()
+  add_child(player)
+  player.stream = sound
+  player.play()
+  player.finished.connect(player.queue_free)
 
 func play_music(music: AudioStream):
-	if not music:
-		return
+  if not music:
+    return
 
-	music_player.stream = music
-	music_player.play()
+  music_player.stream = music
+  music_player.play()
 
 func stop_music():
-	music_player.stop()
+  music_player.stop()
 
 # Convenience functions
 func play_starting_sound():
-	play_sound(starting_sound)
+  play_sound(starting_sound)
 
 func play_intro_music():
-	play_music(intro_music)
+  play_music(intro_music)
 
 func play_background_music():
-	play_music(background_music)
+  play_music(background_music)
